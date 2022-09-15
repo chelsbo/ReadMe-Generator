3c78ce8ae0727f200e44f6ebc150ae9df15ee7f2
chelsbo <chelsea.reese@icloud.com>
2022-09-15 14:10:01 -0600

    Made changes to Index.JS to start project

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,76 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { message } = require('statuses');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ([
+    {
+        type: 'input',
+        message: '',
+        name:'',
+        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+    },
+    {
+        type: 'input',
+        message: '',
+        name:'',
+        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+    },
+    {
+        type: 'input',
+        message: '',
+        name:'',
+        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+    },
+    {
+        type: 'input',
+        message: '',
+        name:'',
+        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+    },
+    {
+        type: 'input',
+        message: '',
+        name:'',
+        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+    },
+    {
+        type: 'input',
+        message: '',
+        name:'',
+        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+    },
+];
+).then(({
+    titles
+})=>{
+    const template = `# ${title}
+    * [Installation]($installation)
+    * [Usage]($usage)
+    * [Contribution]($contribution)
+    * [Credits]($credits)
+    * [License]($license)
+    # Installation
+    ${installation}
+    ## Usage
+    ${usage}
+    ## Contribution
+    ${contribution}
+    ## Instructions
+    ${instructions}
+    ## Credits
+    ${credits}
+    ## License
+    ${license}
 
+    # Contact
+    *GitHub:${git}
+    *LinkedIn :${linkedin}
+    #Email: ${email}`;
+
+}
+)
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
