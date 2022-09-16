99e80fd0d9853cb422e28fdc63a8d4390f35e8a0
chelsbo <chelsea.reese@icloud.com>
2022-09-15 20:17:29 -0600

    Made changes to JS

--- Develop/index.js
+++ Develop/index.js
@@ -9,111 +9,183 @@ const questions = ([
         type: 'input',
         message: 'title',
         name:'What is the name of your application?',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'What is a brief description of your application?',
         name:'description',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'What was your motivation for this application?',
         name:'motivation',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'Why did you build this application?',
         name:'why',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'What problem does it solve?',
         name:'solved',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'What did you learn with creating this application?',
         name:'what',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'What steps are required to install your application?',
         name:'install',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'Are there any collaborators on this application?',
         name:'collabs',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'Did you use any licenses for this application?',
         name:'licenses',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'What is your GitHub?',
         name:'github',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'What is your LinkedIn?',
         name:'linkedin',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
     {
         type: 'input',
         message: 'What is your email?',
         name:'email',
-        validate: (value)=>{ if(value){return true} else{return 'Invalid Value'}},
+        validate: (value)=>{ 
+            if(value)
+            {return true;
+            } else{
+                console.log('Invalid Value')
+            }
+        },
     },
 ])
-then(({
-    titles
-})=>{
-    const template = `# ${title}
-    * [Description]($description)
-    * [Motivation]($motivation)
-    * [Why]($why)
-    * [Solved]($solved)
-    * [What]($what)
-    * [Install]($install)
-    * [Collabs]($collabs)
-    * [Licenses]($licenses)
-    # Description
-    ${description}}
-    ## Motivation
-    ${motivation}
-    ## Why
-    ${why}
-    ## Solved
-    ${solved}
-    ## What
-    ${what}
-    ## Install
-    ${Install}
-    ## Collabs
-    ${collabs}
-    ##Licenses
-    ${licenses}
+// then(({
+//     titles
+// })=>{
+//     const template = `# ${title}
+//     * [Description]($description)
+//     * [Motivation]($motivation)
+//     * [Why]($why)
+//     * [Solved]($solved)
+//     * [What]($what)
+//     * [Install]($install)
+//     * [Collabs]($collabs)
+//     * [Licenses]($licenses)
+//     # Description
+//     ${description}}
+//     ## Motivation
+//     ${motivation}
+//     ## Why
+//     ${why}
+//     ## Solved
+//     ${solved}
+//     ## What
+//     ${what}
+//     ## Install
+//     ${Install}
+//     ## Collabs
+//     ${collabs}
+//     ##Licenses
+//     ${licenses}
 
-    # Contact
-    *GitHub:${git}
-    *LinkedIn :${linkedin}
-    #Email: ${email}`;
+//     # Contact
+//     *GitHub:${git}
+//     *LinkedIn :${linkedin}
+//     #Email: ${email}`;
 
-}
-)
+// }
+// )
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(`./${fileName.toLowerCase(x).split('').join('').md ,data,(err)=>{
@@ -125,7 +197,13 @@ console.log('Your ReadMe has been generated');
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPromptModule(questions)
+    .then(function (userInput){
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
-// init();
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,77 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {""}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {""}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {""}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+    * [Description]($description)
+    * [Motivation]($motivation)
+    * [Why]($why)
+    * [Solved]($solved)
+    * [What]($what)
+    * [Install]($install)
+    * [Collabs]($collabs)
+    * [Licenses]($licenses)
+    # Description
+    ${description}}
+    ## Motivation
+    ${motivation}
+    ## Why
+    ${why}
+    ## Solved
+    ${solved}
+    ## What
+    ${what}
+    ## Install
+    ${Install}
+    ## Collabs
+    ${collabs}
+    ##Licenses
+    ${licenses}
 
-`;
+    # Contact
+    *GitHub:${git}
+    *LinkedIn :${linkedin}
+    #Email: ${email}`;
+
 }
 
 module.exports = generateMarkdown;
+then(({
+  titles
+})=>{
+  const template = `# ${title}
+ 
+  # Description
+  ${description}}
+  ## Motivation
+  ${motivation}
+  ## Why
+  ${why}
+  ## Solved
+  ${solved}
+  ## What
+  ${what}
+  ## Install
+  ${Install}
+  ## Collabs
+  ${collabs}
+  ##Licenses
+  ${licenses}
+
+  # Contact
+  *GitHub:${git}
+  *LinkedIn :${linkedin}
+  #Email: ${email}`;
+
+}
+)
